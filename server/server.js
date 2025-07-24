1a6114051033d10171ce1307ef56e9a0a4f6d5b6
sofoniasj <104975131+sofoniasj@users.noreply.github.com>
2025-07-24 14:06:36 +0300

    sdfs

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://rewrite-9ers.onrender.com', // Vite default client port
+  origin: process.env.CLIENT_URL || 'rewrite-9ers.onrender.com', // Vite default client port
   credentials: true
 }));
 app.use(helmet());
