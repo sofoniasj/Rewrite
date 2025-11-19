019a32ea38a7d8bfb7c49b371d2b7eb98b1782a7
sofoniasj <104975131+sofoniasj@users.noreply.github.com>
2025-11-19 18:14:35 +0300

    ewew

--- client/src/components/Layout/Navbar.jsx
+++ client/src/components/Layout/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="container">
         <Link to="/" className="navbar-brand">
-          Rewrite
+          Draft
         </Link>
         <ul className="nav-links">
           <li>
